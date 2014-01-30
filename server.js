20c97b8b630c9718dc1d7602a1e23cf93ed2ee57
martinkalmus <martin.kalmus@seznam.cz>
2014-01-30 15:38:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 console.log('Nacteni konfigurace, databaze, lokalizace ap.');
 http.createServer(function (req, res) {
   console.log('Zpracovani pozadavku z URL: ' + req.url);
-  res.end('Hello World');
+  res.end('Hello svete');
 }).listen(1337);
